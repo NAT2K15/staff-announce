fad22f2de29189acb7ed5b1cafba1e5b5d4b8583
NAT2K15 <72643615+NAT2K15@users.noreply.github.com>
2024-07-01 19:55:32 -0400

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 config = {}
 
 config.frameworkName = "framework" -- framework name
-config.deptName = "Staff" -- Staff dept name
+config.command = "announce" -- command name
+config.deptLevel = "admin_level" -- Staff dept name
 config.length = 7 -- in seconds. For how long it will appear
--- server/server.lua
+++ server/server.lua
@@ -1,12 +1,8 @@
-RegisterCommand("announce", function(source, args, message) 
-    local src = source
-    local player = exports[config.frameworkName]:getdept(src)
-    if(player == nil or player[src].dept ~= config.deptName) then
-        TriggerClientEvent('chatMessage', -1, "^1Access Denied.")
-    else 
-        local length = string.len(table.concat(args, " "));
-        if(length > 0) then
-            TriggerClientEvent('NAT2K15:STAFFANNOUNCE', -1, table.concat(args, " "))
-        end
+Framework = exports[config.frameworkName]:getServerFunctions()
+Framework.RegisterCommand(config.command, config.deptLevel, nil, function(source, args, message, player) 
+    local length = string.len(table.concat(args, " "));
+    if(length > 0) then
+        TriggerClientEvent('NAT2K15:STAFFANNOUNCE', -1, table.concat(args, " "))
     end
 end)
+
